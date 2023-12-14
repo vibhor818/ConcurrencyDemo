f4a49b9c48d5e17064d75739b5e6237c218e2f3d
vibhoraz400 <vibhoraz400@outlook.com>
2023-12-14 16:25:58 +0530

    Add project files.

---
+++ Migrations/20231214085946_VBAddedProduct.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConcurrencyDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class VBAddedProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Timestamp = table.Column<byte[]>(type: "rowversion", rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Emit;
+
+using(VBContext context=new VBContext())
+{
+    //store win
+    //try
+    //{
+    //    var pro = await context.Products.FindAsync(1);
+    //    pro.Name = "Mobile";
+    //    await context.SaveChangesAsync();
+    //    Console.WriteLine("Changes saved successfully.");
+    //}
+    //catch (DbUpdateConcurrencyException ex)
+    //{
+    //    foreach (var entry in ex.Entries)
+    //    {
+    //        //var databaseValues = await entry.GetDatabaseValuesAsync();
+    //        //var clientValues = entry.CurrentValues.Clone();
+
+    //        //entry.OriginalValues.SetValues(databaseValues);
+    //        //await context.SaveChangesAsync();
+    //        Console.WriteLine("Server wins, changes done.");
+    //    }
+
+    //}
+    //client wins
+
+    try
+    {
+        var pro = await context.Products.FindAsync(1);
+        pro.Name = "TV";
+        await context.SaveChangesAsync();
+        Console.WriteLine("Changes saved successfully.");
+    }
+    catch (DbUpdateConcurrencyException ex)
+    {
+        foreach (var entry in ex.Entries)
+        {
+            var databaseValues = await entry.GetDatabaseValuesAsync();
+            if (databaseValues == null)
+            {
+                Console.WriteLine("record deleted");
+            }
+            else
+            {
+                entry.OriginalValues.SetValues(databaseValues);
+                await context.SaveChangesAsync();
+                Console.WriteLine("Cleint wins, changes done.");
+            }
+        }
+
+     }
+}
+
+public class VBContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"data source=(localdb)\mssqllocaldb;database=concurrentdb;trusted_connection=true");
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Timestamp)
+            .IsRowVersion();
+    }
+    public DbSet<Product> Products { get; set; }
+}
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+    [Timestamp]
+    public byte[] Timestamp { get; set; }
+
+    //nosql
+    //[ConcurrencyCheck]
+    //public Guid MyProperty { get; set; }
+}
